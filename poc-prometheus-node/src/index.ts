4a908a055279decdd7e17d857a8b2805eef05869
Arthur Soares <arthur.soares@ifood.com.br>
2022-11-16 20:37:32 -0300

    update node example

--- poc-prometheus-node/src/index.ts
+++ poc-prometheus-node/src/index.ts
@@ -1,9 +1,49 @@
-import express, { Express, Request, Response } from 'express';
+import express, {Express, Request, Response} from 'express';
 
 const app: Express = express();
 const port = 8089;
 
-app.get('/', (req: Request, res: Response) => {
+type CatFoodBill = {
+    value: number,
+    catId: number,
+    catName: string
+}
+
+type CatFood = {
+    name: string,
+    price: number,
+    stock: number
+}
+
+const bills: CatFoodBill[] = []
+
+const availableFoods: CatFood[] = [
+    {
+        name: 'peixe',
+        price: 1.5,
+        stock: 100
+    },
+    {
+        name: 'whiskas sache',
+        price: 3,
+        stock: 10
+    },
+    {
+        name: 'rato',
+        price: 10,
+        stock: 4
+    },
+    {
+        name: 'lagartixa',
+        price: 100,
+        stock: 2
+    }
+]
+
+
+app.post('/cat/food/:catId', async (req: Request, res: Response) => {
+    let response = await fetch(`http://localhost:8080/cats/${req.params["catId"]}`);
+    console.log(await response.json());
     res.send('Express + TypeScript Server');
 });
 
